1a33f5b7ee0464b4dee97d71c046566a720f8a75
SaulSandovalM <sauldevelop@gmail.com>
2017-09-20 13:09:46 -0500

    cambios

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyBH8MO4YDgkvQ28wQ4TVGMWZk0JkPCB8_k",
-  authDomain: "reactfixter.firebaseapp.com",
-  databaseURL: "https://reactfixter.firebaseio.com",
-  projectId: "reactfixter",
-  storageBucket: "reactfixter.appspot.com",
-  messagingSenderId: "430923296727"
+  apiKey: "AIzaSyAm3vwjUC-pSCOnwRZTirbXP-PKJ8NJ8dc",
+  authDomain: "planb-ab808.firebaseapp.com",
+  databaseURL: "https://planb-ab808.firebaseio.com",
+  projectId: "planb-ab808",
+  storageBucket: "planb-ab808.appspot.com",
+  messagingSenderId: "558603502686"
 };
 firebase.initializeApp(config);
 export const firebaseAuth = firebase.auth()
